dae90e9ca12cb6e7aec7154ef0700eae431dffb6
Damian Chojnacki <daamian3@gmail.com>
2021-09-14 12:57:53 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 cp .env.example .env
+./vendor/bin/sail build
 ./vendor/bin/sail up -d
 sleep 5
 ./vendor/bin/sail composer install && ./vendor/bin/sail down && ./vendor/bin/sail up -d
 ./vendor/bin/sail artisan migrate:fresh --seed
+echo "Copy and paste key below to APP_KEY= in the .env file:"
 ./vendor/bin/sail artisan key:generate --show
